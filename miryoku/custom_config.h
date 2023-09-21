fcdd3d187b9fea4af54f38a878613184c3e90c8d
David Liechty <qwertyshoe@gmail.com>
2023-09-20 22:59:14 -0700

    Fix mapping

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -154,7 +154,7 @@
 #define THMB(MOD, TAP) &thumb MOD TAP
 
 #define MIRYOKU_LAYER_BASE \
-&kp W,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,              \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,              \
 HMY_L(LGUI, A),    HMY_L(LALT, R),    HMY_L(LCTRL, S),   IND_L(LSHFT, T),   &kp G,             &kp M,             IND_R(LSHFT, N),   HMY_R(LCTRL, E),   HMY_R(LALT, I),    HMY_R(LGUI, O),       \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              THMB(U_MEDIA, ESC),THMB(U_NAV, SPACE),THMB(U_MOUSE, TAB),THMB(U_SYM, RET),  THMB(U_NUM, BSPC), THMB(U_FUN, DEL),  U_NP,              U_NP
